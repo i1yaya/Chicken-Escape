9c92da38bd69a01d8f05cb019fa9245296056a01
i1yaya <2665432168910zxcVBN>
2025-07-07 23:22:07 +0300

    Initial Commit

---
+++ ChuckWayOut/ChuckWayOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckWayOutApp.swift
+//  ChuckWayOut
+//
+//  Created by Ilya Ruyatkin on 07.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChuckWayOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
